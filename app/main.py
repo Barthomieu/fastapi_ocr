26f0114ba062b60a6762f1ec708a74816158a174
Bartłomiej <bartlomiej.plata@edu.uekat.pl>
2023-01-15 13:55:28 +0100

    fix: new file reading machanism

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import os
 import shutil
 import uvicorn
-import app.utils.image_ocr as ocr
-from fastapi import FastAPI, Request, UploadFile, File
+import base64
+from utils.image_ocr import *
+from fastapi import FastAPI, Request, UploadFile, File, Depends, HTTPException
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -18,8 +20,35 @@ def home(request: Request):
 @app.post("/extract_text")
 async def perform_ocr(image: UploadFile = File(...)):
     temp_file = _save_file_to_disc(image, path="app/temp", save_as="temp")
-    text = await ocr.read_image(temp_file)
-    return {"filename": image.filename, "text": text}
+    text = await read_image(temp_file)
+    try:
+        contents = image.file.read()
+        with open("uploaded_" + image.filename, "wb") as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        image.file.close()
+
+    base64_encoded_image = base64.b64encode(contents).decode("utf-8")
+    return {"filename": image.filename, "text": text, "myImage": base64_encoded_image}
+
+@app.post("/convert")
+async def post_image_to_text(file: UploadFile = File(...)):
+    img = await read_img(file, HTTPException(status_code=422, detail="Invalid image"))
+
+    ocr_predictions: str = apply_ocr(img)
+    print(ocr_predictions)
+    return {
+        "results": {
+            "raw": ocr_predictions,
+            "cleaned": ocr_predictions.replace("\n", " ").strip(),
+            "lines": ocr_predictions.split("\n"),
+        }
+    }
+
+
+
 
 def _save_file_to_disc(uploaded_file, path=".", save_as="default"):
     extension = os.path.splitext(uploaded_file.filename)[-1]
--- app/utils/image_ocr.py
+++ app/utils/image_ocr.py
@@ -1,7 +1,12 @@
 import pytesseract
 import os
 import sys
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+import io
+import cv2
+from PIL import Image
+from numpy import asarray, ndarray, ones, uint8
+from fastapi import UploadFile
+#pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 async def read_image(img_path, lang='eng'):
     """
@@ -43,3 +48,73 @@ def _write_to_file(text, file_path):
     print("[INFO] Writing text to file: {0}".format(file_path))
     with open(file_path, 'w') as fp:
         fp.write(text)
+
+
+async def read_img(img: UploadFile, read_exception):
+    # Convert file into a byte stream
+    byte_string = io.BytesIO(await img.read())
+
+    # Validate image by attempting to open it with PIL
+    try:
+        img = Image.open(byte_string)
+    except:
+        raise read_exception
+
+    # Convert to numpy array for opencv
+    img_array = asarray(img)
+    print("obraz po przetworzeniu", img_array)
+    return img_array
+
+
+def resize_img(img_array: ndarray):
+    # Resize image
+    return cv2.resize(img_array, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+
+
+def convert_to_grayscale(img_array):
+    img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+
+    return img
+
+
+def denoise_img(img_array):
+    kernel = ones((1, 1), uint8)
+    img = cv2.dilate(img_array, kernel, iterations=1)
+    img = cv2.erode(img_array, kernel, iterations=1)
+
+    return img
+
+
+def blur_img(img_array):
+    # Extract & blur the bg so that the text is highlighted
+    bg = cv2.threshold(
+        cv2.medianBlur(img_array, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )[1]
+
+    img = 255 - cv2.absdiff(img_array, bg)
+
+    return img
+
+
+def apply_ocr(img_array: ndarray):
+    # Image processing for better predictions
+
+    # 1. Resize image to 300 dpi
+    img = resize_img(img_array)
+
+    # 2. Convert image to grayscale
+    img = convert_to_grayscale(img)
+
+    # 3. Remove noise
+    img = denoise_img(img)
+
+    # 4. Blur & segment image
+    img = blur_img(img)
+
+
+    try:
+        preds: str = pytesseract.image_to_string(img)  
+        return preds
+    except:
+        return "[ERROR] Unable to process file)"
+   
