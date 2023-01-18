363b8e53f52a6b89eb052180a9763976d2b34179
Bartłomiej <bartlomiej.plata@edu.uekat.pl>
2023-01-18 22:44:55 +0100

    fix: fix some flake8 code violation

--- app/test/ocr_test.py
+++ app/test/ocr_test.py
@@ -1,26 +1,20 @@
 import PIL
-
 import app.utils.image_ocr as ocr
-import pytesseract
-from PIL import Image
-from app.utils.image_preprocesing import preprocess_image
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-
 from os.path import exists
+from PIL import Image
+from app.utils.image_preprocesing import *
 
+# pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 path = r'../temp/ocrTest3.jpg'
 print(exists(path))
-
-
-image = preprocess_image(path)
 
 text = ocr.read_image(path)
 print(text)
 
 
-def process_image(image):
-    return pytesseract.image_to_string(image)
+def process_image(image1):
+    return pytesseract.image_to_string(image1)
 
 
 image = PIL.Image.open(path)
